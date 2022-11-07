dd3aa3bac57dc53da30c7c756020ecebb4eaa206
Erik Haraldson <erikharaldson@macbook-pro-9.home>
2022-11-07 16:01:10 +0000

    Adds get_winner function to manual_rps

--- manual_rps.py
+++ manual_rps.py
@@ -10,3 +10,11 @@ def get_user_choice():
         user_choice = input('Rock... Paper... Scissors... Shoot: ')
         if user_choice not in options:
             print('Please enter a valid input')
+
+def get_winner(computer_choice, user_choice):
+    if (computer_choice == 'rock' and user_choice == 'scissors') or (computer_choice == 'paper' and user_choice == 'rock') or (computer_choice == 'scissors' and user_choice == 'paper'):
+        print("You lost")
+    elif computer_choice == user_choice:
+        print("It is a tie!")
+    else:
+        print("You won!")

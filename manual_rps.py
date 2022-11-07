fdca01cce33722378f7fb58e76bedc63bb8a0436
Erik Haraldson <erikharaldson@macbook-pro-9.home>
2022-11-07 16:06:27 +0000

    Wraps the functions in play function to play the game manually

--- manual_rps.py
+++ manual_rps.py
@@ -10,11 +10,22 @@ def get_user_choice():
         user_choice = input('Rock... Paper... Scissors... Shoot: ')
         if user_choice not in options:
             print('Please enter a valid input')
+        else:
+            return user_choice
 
 def get_winner(computer_choice, user_choice):
-    if (computer_choice == 'rock' and user_choice == 'scissors') or (computer_choice == 'paper' and user_choice == 'rock') or (computer_choice == 'scissors' and user_choice == 'paper'):
+    if (computer_choice == 'Rock' and user_choice == 'Scissors') or (computer_choice == 'Paper' and user_choice == 'Rock') or (computer_choice == 'Scissors' and user_choice == 'Paper'):
         print("You lost")
     elif computer_choice == user_choice:
         print("It is a tie!")
     else:
         print("You won!")
+
+def play():
+
+    computer_choice = get_computer_choice()
+    user_choice = get_user_choice()
+
+    get_winner(computer_choice, user_choice)
+
+play()
